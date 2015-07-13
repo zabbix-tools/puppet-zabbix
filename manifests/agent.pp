a1183b7ae8c88f3c45f11e547f111e810f05f636
Ryan Armstrong <ryan@cavaliercoder.com>
2015-07-13 13:04:28 +0800

    Enabled custom agent package source and provider

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,15 +1,25 @@
 class zabbix::agent (
-  $ensure = 'present',
-  $package = $::zabbix::params::agent_package,
-  $package_version = $::zabbix::params::package_version,
+  $ensure           = 'present',
+  $package          = $::zabbix::params::agent_package,
+  $package_version  = $::zabbix::params::package_version,
+  $package_source   = undef,
+  $package_provider = undef,
 ) inherits zabbix::params {
   case $ensure {
     # install zabbix agent
     'present': {
       require ::zabbix::repo
 
+      # which provider should we use?
+      $_package_provider = $package_provider ? {
+        undef   => $package_source ? { undef => 'yum', default => 'rpm' },
+        default => $package_provider,
+      }
+
       package { $package :
         ensure        => $package_version,
+        provider      => $_package_provider,
+        source        => $package_source,
         allow_virtual => false,
       }
 

d1985355d352708ac601d1a1823d9a311038d479
Ryan Armstrong <ryan@cavaliercoder.com>
2016-10-17 10:19:44 +0800

    Added .pgpass credentials

--- manifests/server/database.pp
+++ manifests/server/database.pp
@@ -5,6 +5,7 @@ class zabbix::server::database {
       'pgsql' : {
         $setup_bin = '/usr/local/bin/setup_zabbix_pgsql_database.sh'
 
+        # install db configuration script
         file { $setup_bin :
           ensure  => 'file',
           owner   => 0,
@@ -16,15 +17,25 @@ class zabbix::server::database {
           selrole => 'object_r',
         } ->
 
+        # configure database if not configured
         exec { 'setup zabbix database' :
           command     => $setup_bin,
           unless      => "${setup_bin} check",
           path        => '/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin',
         }
+
+        # install ~/.pgpass credentials
+        file { "${::zabbix::server::user_home}/.pgpass" :
+          ensure  => 'file',
+          content => template("${module_name}/pgpass.erb"),
+          owner   => $::zabbix::server::user_name,
+          group   => $::zabbix::server::user_group,
+          mode    => '0600',
+        }
       }
 
       default : {
-        fail("Unsupported database driver: ${::zabbix::server::database_engine}")
+        fail("Unsupported database driver: ${::zabbix::server::database_driver}")
       }
     }
   }
